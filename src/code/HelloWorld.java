8fb12b144ba6030e05aafefc11393574fec78878
Jonathan Doyle <jdoyle@guidewire.com>
2016-12-07 16:28:09 +0000

    initial commit

---
+++ src/code/HelloWorld.java
@@ -0,0 +1,10 @@
+package code;
+
+/**
+ * Created by jdoyle on 07/12/2016.
+ */
+public class HelloWorld {
+    public static String message(){
+        return "hello world";
+    }
+}
---
+++ src/test/HelloWorldTest.java
@@ -0,0 +1,17 @@
+package test;
+
+import code.HelloWorld;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jdoyle on 07/12/2016.
+ */
+public class HelloWorldTest {
+    @Test
+    public void message() throws Exception {
+        assertEquals("hello world", HelloWorld.message());
+    }
+
+}
